0cae547740fd3619bce166031e226049719c991a
abdullahselek <abdullahselek@gmail.com>
2017-02-07 21:04:55 +0100

    - create project

---
+++ WatchCon/WatchCon/WatchCon.h
@@ -0,0 +1,19 @@
+//
+//  WatchCon.h
+//  WatchCon
+//
+//  Created by Abdullah Selek on 07/02/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WatchCon.
+FOUNDATION_EXPORT double WatchConVersionNumber;
+
+//! Project version string for WatchCon.
+FOUNDATION_EXPORT const unsigned char WatchConVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchCon/PublicHeader.h>
+
+
---
+++ WatchCon/WatchConTests/WatchConTests.m
@@ -0,0 +1,39 @@
+//
+//  WatchConTests.m
+//  WatchConTests
+//
+//  Created by Abdullah Selek on 07/02/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WatchConTests : XCTestCase
+
+@end
+
+@implementation WatchConTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
