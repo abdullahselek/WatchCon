62c6ea20f81c8d598ec17afa0d0d6e9797a0bb91
abdullahselek <abdullahselek@gmail.com>
2017-02-17 20:19:30 +0100

    - add watchcon dependency with iOS target

--- Sample/WatchOS-Sample/WatchOS-Sample/ViewController.h
+++ Sample/WatchOS-Sample/WatchOS-Sample/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WatchCon/WatchCon.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<WatchConSessionDelegate>
 
 
 @end
--- Sample/WatchOS-Sample/WatchOS-Sample/ViewController.m
+++ Sample/WatchOS-Sample/WatchOS-Sample/ViewController.m
@@ -19,6 +19,23 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - WatchConSession Delegate
+
+- (void)didReceiveApplicationContext:(NSDictionary<NSString *, id> *)applicationContext {
+
+}
+
+- (void)didReceiveUserInfo:(NSDictionary<NSString *, id> *)userInfo {
+
+}
+
+- (void)didFinishFileTransfer:(WCSessionFileTransfer *)fileTransfer error:(nullable NSError *)error {
+
+}
+
+- (void)didReceiveFile:(WCSessionFile *)file {
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
